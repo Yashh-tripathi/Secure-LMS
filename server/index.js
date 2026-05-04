50b6b85e331a7d5db64471ff668f40e82c329392
Yashh-tripathi <yashtripathi980@gmail.com>
2026-05-04 17:22:40 +0530

    add security and performance enhancements with middleware integration

--- server/index.js
+++ server/index.js
@@ -1,6 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
+import {rateLimit} from 'express-rate-limit';
+import helmet from 'helmet';
+import ExpressMongoSanitize from 'express-mongo-sanitize';
+import hpp from 'hpp';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 dotenv.config({
     path: ""
@@ -9,6 +15,22 @@ dotenv.config({
 
 const app = express();
 const PORT = process.env.PORT
+
+//Global rate limiter
+const limiter = rateLimit({
+    windowMs: 15*60*1000,
+    limit: 100,
+    message: "Too many messages for this IP, please try later.",
+    // standardHeaders: 'draft-8',
+    // legacyHeaders: false,
+    // ipv6Subnet: 60
+});
+
+//security middleware
+app.use(helmet());
+app.use(ExpressMongoSanitize());
+app.use(hpp());
+app.use(limiter);
 
 //logger middleware
 if(process.env.NODE_ENV === 'development'){
@@ -20,6 +42,7 @@ if(process.env.NODE_ENV === 'development'){
 //body parser middlewares
 app.use(express.json({limit: '16kb'}));
 app.use(express.urlencoded({extended: true, limit: '10kb'}));
+app.use(cookieParser());
 
 //Global Error handler
 app.use((err, req,res,next) => {
@@ -30,6 +53,23 @@ app.use((err, req,res,next) => {
         ...(process.env.NODE_ENV === 'development' && {stack: err.stack})
     });
 });
+
+
+//CORS configuration
+app.use(cors({
+    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD','OPTIONS'],
+    allowedHeaders: [
+        "Content-Type",
+        "Authorization",
+        "X-Requested-With",
+        "device-remember-token",
+        "Access-Control-Allow-Origin",
+        "Origin",
+        "Accept",
+    ]
+}))
 
 
 //API Routes
